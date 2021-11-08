a59d0919fa9ccf9c612f82a81f14c4faff2d076e
slashburn3 <93943677+slashburn3@users.noreply.github.com>
2021-11-08 14:12:52 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,13 @@
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.clear_screen()
+    basic.pause(500)
+    basic.show_icon(IconNames.HEART)
+    basic.clear_screen()
+    basic.pause(500)
+    basic.show_icon(IconNames.HEART)
+    basic.clear_screen()
+    basic.pause(500)
+
+while True:
+    on_forever()
---
+++ main.ts
@@ -0,0 +1,15 @@
+function on_forever() {
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.pause(500)
+}
+
+while (true) {
+    on_forever()
+}
